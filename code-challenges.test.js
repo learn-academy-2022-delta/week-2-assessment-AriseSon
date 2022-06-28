912c548002542afa768e123a7811b030a847510b
Ricky Fort <100113249+AriseSon@users.noreply.github.com>
2022-06-28 09:28:56 -0700

    complted assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,69 +1,316 @@
-// ASSESSMENT 2: Coding Practical Questions with Jest
+// // ASSESSMENT 2: Coding Practical Questions with Jest
 
-// Please read all questions thoroughly
-// Pseudo coding is REQUIRED
-// If you get stuck, please leave comments to help us understand your thought process
 
-// Use test driven development to complete the following questions
-// Add appropriate dependencies: $ yarn add jest
+// // Please read all questions thoroughly
+// // Pseudo coding is REQUIRED
+// // If you get stuck, please leave comments to help us understand your thought process
 
-// Reminder: The test will call your function
-// Run the file with the following command: $ yarn jest
+// // Use test driven development to complete the following questions
+// // Add appropriate dependencies: $ yarn add jest
 
-// --------------------INSTRUCTOR EXAMPLE: Create a function that takes in an array of numbers and returns an array with all the numbers multipled by 3.
+// // Reminder: The test will call your function
+// // Run the file with the following command: $ yarn jest
 
-// a) Create a test with expect statements for each of the variables provided.
+// // --------------------INSTRUCTOR EXAMPLE: Create a function that takes in an array of numbers and returns an array with all the numbers multipled by 3.
+
+// // a) Create a test with expect statements for each of the variables provided.
 
 const numbersArray1 = [6, 7, 8, 9, 10]
-// Expected output: [18, 21, 24, 27, 30]
+// // Expected output: [18, 21, 24, 27, 30]
 const numbersArray2 = [24, 27, 30, 33, 36]
-// Expected output: [72, 81, 90, 99, 108]
+// // Expected output: [72, 81, 90, 99, 108]
 
 
-// b) Create the function that makes the test pass.
+// // b) Create the function that makes the test pass.
+
+const timesThree = numbersArray1.map(value => {
+  return value * 3
+})
+
+console.log(timesThree)
 
 
 
-// --------------------1) Create a function that takes a number as an argument and decides if the number is evenly divisble by three or not.
+//1//
+// // --------------------1) Create a function that takes a number as an argument and decides if the number is evenly divisble by three or not.
 
-// a) Create a test with expect statements for each of the variables provided.
+// // a) Create a test with expect statements for each of the variables provided.
 
 const num1 = 15
-// Expected output: "15 is divisible by three"
+// // Expected output: "15 is divisible by three"
 const num2 = 0
-// Expected output: "0 is divisible by three"
+// // Expected output: "0 is divisible by three"
 const num3 = -7
-// Expected output: "-7 is not divisible by three"
+// // Expected output: "-7 is not divisible by three"
+
+//----- MY TEST -------
+
+describe("create a function call it outcome", () => {
+
+    it("use if statement to check if num1 is divisible by three with no remainder, if statement is true then log num1 is divisible by three, if statement is false then log num2 is not divisible by three", () => {
+ 
+      expect(outcome(num1)).toEach("15 is divisible by three")
+    })
+})      
+
+describe("create a function call it outcome", () => {
+
+    it("use if statement to check if num2 is divisible by three with no remainder, if statement is true then log num2 is divisible by three, if statement is false then log num2 is not divisible by three", () => {
+ ""
+      expect(outcome(num2)).toEach("0 is divisible by three")
+    })
+}) 
+
+describe("create a function call it outcome", () => {
+
+    it("use if statement to check if num3 is divisible by three with no remainder, if statement is true then log num3 is divisible by three, if statement is false then log num2 is not divisible by three", () => {
+ 
+      expect(outcome(num3)).toEach("-7 is not divisible by three")
+    })
+})
 
 
-// b) Create the function that makes the test pass.
+
+// // b) Create the function that makes the test pass.
+
+
+//----- I created a functiion called outcome
+
+const outcome = () => {
+
+//----- I used a if statement to return num1 as (`${num1} is divisible by three`) if num1 is divisible by 3 with no remainder
+
+     if (num1 % 3 == 0) {
+   
+   
+       return (`${num1} is divisible by three`)
+      } 
+      else {
+   
+//----- If else, return (`${num1} is not divisible by three`)
+
+       return (`${num1} is not divisible by three`)
+       }
+    } 
+
+//----- console.log(result)
+      
+console.log(outcome())
+
+//------ My Expected Output : "15 is divisible by three"
+//------ My Output : "15 is divisible by three"
+
+
+// const outcome = () => {
+
+//     if (num2 % 3 == 0) {
+   
+//         return (`${num2} is divisible by three`)
+    
+//     } 
+//    else 
+//     {
+//    return (`${num2} is not divisible by three`)
+//      
+// }
+//}
+//----- console.log(result)
+      
+console.log(outcome())
+
+//------ My Expected Output : "0 is divisible by three"
+//------ My Output : "0 is divisible by three"
 
 
 
-// --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
+// const outcome = () => {
 
-// a) Create a test with expect statements for each of the variables provided.
+//     if (num3 % 3 == 0) {
+   
+//         return (`${num3} is divisible by three`)
+    
+//     } 
+//    else 
+//     {
+//    return (`${num3} is not divisible by three`)
+//      
+// }
+//} 
+//----- console.log(result)
+      
+console.log(outcome())
+
+//------ My Expected Output : "-7 is not divisible by three"
+//------ My Output : "-7 is not divisible by three"
+
+
+
+//2//
+// // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
+
+// // a) Create a test with expect statements for each of the variables provided.
 
 const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
-// Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
+// // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
 const randomNouns2 = ["temperature", "database", "chopsticks", "mango", "deduction"]
-// Expected output: ["Temperature", "Database", "Chopsticks", "Mango", "Deduction"]
+// // Expected output: ["Temperature", "Database", "Chopsticks", "Mango", "Deduction"]
+
+//------ MY TEST----------------
+
+describe("capFirst", () => {
+
+    it("use map to pass through an array while capitalizing index 0 from randomNouns1, add substring to stop toUpperCase at index 1, then join array", () => {
+   
+     expect(capFirst(randomNouns1)).toEqual("Streetlamp Potato Teeth Conclusion Nephew")
+   
+      })
+   })
+
+describe("capFirst", () => {
+
+    it("use map to pass through an array while capitalizing index 0 from randomNouns2, add substring to stop toUpperCase at index 1, then join array", () => {
+   
+     expect(capFirst(randomNouns2)).toEqual("Temperature Database Chopsticks Mango Deduction")
+   
+      })
+   })
 
 
-// b) Create the function that makes the test pass.
+// // b) Create the function that makes the test pass.
+
+
+//----- I created a function called capFirst
+
+const capFirst = () => {
+
+//----- I used the .map() to locate and capitalize .toUpperCase the index of the first letter of each word in randomNouns1 and randomNouns2
+    
+  let firstCap = randomNouns2.map(value => {
+
+    return value.toUpperCase() + value.substring(1)
+    })
+
+//----- I used the .join(" ") to get the array of strings to display as expected    
+
+    return firstCap.join(" ")
+}
+
+//----- console.log results
+
+console.log(capFirst())
+
+//---- My Expected Output : "Temperature", "Database", "Chopsticks", "Mango", "Deduction"
+//---- My Output : "Temperature", "Database", "Chopsticks", "Mango", "Deduction"
 
 
 
-// --------------------3) Create a function that takes in a string and logs the index of the first vowel.
+// const capFirst = () => {
 
-// a) Create a test with expect statements for each of the variables provided.
+//    let firstCap = randomNouns1.map(value => {
+    
+//     return value.toUpperCase() + value.substring(1) 
+// })
+//      return firstCap.join(" ")
+// }
+// console.log(capFirst())
 
-const vowelTester1 = "learn"
+//---- My Expected Output : "Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"
+//---- My Output : "Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"
+
+
+
+//3//
+// // --------------------3) Create a function that takes in a string and logs the index of the first vowel.
+
+// // a) Create a test with expect statements for each of the variables provided.
+
+ const vowelTester1 = "learn"
 // Expected output: 1
-const vowelTester2 = "academy"
+ const vowelTester2 = "academy"
 // Expected output: 0
-const vowelTester3 = "challenges"
+ const vowelTester3 = "challenges"
 // Expected output: 2
 
 
-// b) Create the function that makes the test pass.
+//------- MY TEST-----------
+
+describe("findFirstVowel", () => { 
+
+ it("use .split to split vowelTester1, then use indexOf to index the first instance of a vowel", () => {
+  
+   expect(findFirstVowel(vowelTester1)).toEqual(1)
+  
+  })
+})
+
+describe("findFirstVowel", () => { 
+
+    it("use .split to split vowelTester2, then use indexOf to index the first instance of a vowel", () => {
+     
+      expect(findFirstVowel(vowelTester2)).toEqual(0)
+     
+     })
+   })
+
+describe("findFirstVowel", () => { 
+
+    it("use .split to split vowelTester3, then use indexOf to index the first instance of a vowel", () => {
+     
+      expect(findFirstVowel(vowelTester3)).toEqual(2)
+     
+     })
+   })
+
+
+// // b) Create the function that makes the test pass.
+
+
+//---- I created a function called findFirstVowel
+
+const findFirstVowel = () => {
+
+//----- I used .split("") to separate the letters in the string
+
+const firstVowel = vowelTester1.split("")
+
+//----- I used .indexOf to reference the first instance of a vowel
+
+    return firstVowel.indexOf("a", "e", "i", "o", "u")  
+
+}
+//----- Console.log the results
+
+console.log(findFirstVowel())
+
+//----- My Expected Output : 1
+//----- My Output : 1
+
+
+
+// const findFirstVowel = () => {
+   
+//     const firstVowel = vowelTester2.split("") 
+    
+//         return firstVowel.indexOf("a", "e", "i", "o", "u")  
+    
+//     }
+    
+//     console.log(findFirstVowel())
+
+//----- My Expected Output : 0
+//----- My Output : 0
+
+
+
+//     const findFirstVowel = () => {
+   
+//         const firstVowel = vowelTester3.split("") 
+        
+//             return firstVowel.indexOf("a", "e", "i", "o", "u")  
+        
+//         }
+        
+//         console.log(findFirstVowel())
+
+//----- My Expected Output : 2
+//----- My Output : 2
--- coding-concepts.js
+++ coding-concepts.js
@@ -8,39 +8,76 @@
 // --------------------1) What will this log?
 
 var cohort = "Delta 2022"
-// console.log(cohort.split(""))
+console.log(cohort.split(""))
 
 // a) Your answer:
+
+// I think this will log 'D', 'e', 'l', 't', 'a', '2', '0', '2', '2' .... I recently used the .split(" ") in problem 3 on week 2
+//                                                                        code challenges- est .... EX vowelTester1.split("") 
+
 // b) Verify and explain:
+
+//---- CONSOLE.LOG ----
+//  'D', 'e', 'l', 't',  .split("") converts a string into an array, .split("") takes an optional argument that describes where the 
+//  'a', ' ', '2', '0',  string string is split. The original string is unchanged 
+//  '2', '2'                         
+
 
 
 // --------------------2) What will this log?
 
 const greeter = (name) => {
   `Hello, ${name}!`
-}
-// console.log(greeter("LEARN Student"))
+}                                                 
+ console.log(greeter("LEARN Student"))
 
 // a) Your answer:
+
+// I think this will log Hello, LEARN Student
+
 // b) Verify and explain:
+
+//---- CONSOLE LOG ----
+//     undefined ......... 
 
 
 // --------------------3) What will this log?
 
 var multBy2 = [4, 5, 6, 7, 8].map(value => value * 2)
-// console.log(multBy2)
+ console.log(multBy2)
 
 // a) Your answer:
+
+// I think this will log [8, 10, 12, 14, 16] .... I recently used the .map() in the practice problem on week two
+//                                                code challenges test .... EX const timesThree = numbersArray1.map(value => {
+//                                                                                                 return value * 3
+                                                                      
 // b) Verify and explain:
+
+//---- CONSOLE LOG ----
+// [ 8, 10, 12, 14, 16 ] .... .map() account for each element in an array, .map() will not execute for empty elements.
+//                             (value => value * 2) multiple the value times two
+
 
 
 // --------------------4) What will this log?
 
 var onlyOdds = [11, 12, 13, 14, 15].filter(value => value % 2 !== 0)
-// console.log(onlyOdds)
+console.log(onlyOdds)
 
 // a) Your answer:
+
+// I think this will log [11, 13, 15] .... I recently used the .filter() in problem 4 on week 1 assignment to account for
+//                                         odd and even numbers.
+//                                         EX .... const myArray = [13, 34, 5, 10, 27, 42].filter(value => value % 2 !== 0)
 // b) Verify and explain:
+
+//----- CONSOLE.LOG-----
+//    [ 11, 13, 15 ] ............ .filter() create a new array with elements that pass a test provided by a function.
+//                                .filter not execute for empty elements, nor does this method change the original array. 
+//                                (value => value % 2 !== 0) the function will keep all numbers that doesnt have a remainder 
+//                                of 0.
+
 
 
 // --------------------5) What will this log?
@@ -51,10 +88,17 @@ var myCodingSkills = {
   databases: "PostgreSQL",
   versionControl: "GitHub"
 }
-// console.log(myCodingSkills.languages[0])
+console.log(myCodingSkills.languages[0])
 
 // a) Your answer:
+
+// I think this will log JavaScript .... 
+
 // b) Verify and explain:
+
+//---- CONSOLE.LOG ----
+//     JavaScript ........ console.log(myCodingSkills.languages[0]) .... Javascript is in the 0 index of the array lanuages,
+//                                                                       inside myCodingSkills 
 
 
 // --------------------STRETCH: What will this log?
@@ -67,7 +111,16 @@ class Learn {
   }
 }
 var learnStudent = new Learn("George")
-// console.log(learnStudent)
+console.log(learnStudent)
 
 // a) Your answer:
+
+// I think this will log  George Bravo 2022
+
 // b) Verify and explain:
+
+//----CONSOLE.LOG ----
+// ---- { student: 'George', cohort: 'Bravo', year: 2022 } .... I was off 
+//                                                              learnStudent equals George added to class .... When the function
+//                                                              invokes, the constructor executes this, which will log 
+//                                                              { student: 'George', cohort: 'Bravo', year: 2022 }
